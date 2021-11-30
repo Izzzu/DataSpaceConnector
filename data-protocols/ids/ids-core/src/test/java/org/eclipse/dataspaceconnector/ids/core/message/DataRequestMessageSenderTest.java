808ab2dcd2393cc6eee22d72780f4fc056be3841
Izabela Kulakowska <ikulakowska@microsoft.com>
2021-11-30 17:23:53 +0100

    fix test

--- data-protocols/ids/ids-core/src/test/java/org/eclipse/dataspaceconnector/ids/core/message/DataRequestMessageSenderTest.java
+++ data-protocols/ids/ids-core/src/test/java/org/eclipse/dataspaceconnector/ids/core/message/DataRequestMessageSenderTest.java
@@ -1,14 +1,12 @@
 package org.eclipse.dataspaceconnector.ids.core.message;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
-import de.fraunhofer.iais.eis.ArtifactRequestMessage;
-import de.fraunhofer.iais.eis.ArtifactRequestMessageBuilder;
-import de.fraunhofer.iais.eis.DynamicAttributeTokenBuilder;
+import de.fraunhofer.iais.eis.ArtifactRequestMessageImpl;
 import okhttp3.*;
+import okio.Buffer;
+import org.easymock.Capture;
 import org.easymock.EasyMock;
-import org.easymock.IArgumentMatcher;
 import org.eclipse.dataspaceconnector.spi.iam.IdentityService;
 import org.eclipse.dataspaceconnector.spi.iam.TokenResult;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
@@ -16,15 +14,16 @@ import org.eclipse.dataspaceconnector.spi.security.Vault;
 import org.eclipse.dataspaceconnector.spi.transfer.store.TransferProcessStore;
 import org.eclipse.dataspaceconnector.spi.types.domain.transfer.DataAddress;
 import org.eclipse.dataspaceconnector.spi.types.domain.transfer.DataRequest;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.Serializable;
+import java.io.IOException;
 import java.net.URI;
-import java.util.HashMap;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+import static java.util.Map.entry;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.easymock.EasyMock.*;
 import static org.eclipse.dataspaceconnector.ids.spi.Protocols.IDS_REST;
 
@@ -33,10 +32,9 @@ class DataRequestMessageSenderTest {
     public static final String DESTINATION_KEY = "dataspaceconnector-data-destination";
     public static final String PROPERTIES_KEY = "dataspaceconnector-properties";
     static Faker faker = new Faker();
-    private final String connectorAddress = "http://"+faker.internet().url();
-    private final String connectorId = faker.internet().uuid();
+    private final String connectorAddress = "http://" + faker.internet().url();
+    private final String connectorId = faker.internet().url();
     private final String processId = faker.internet().uuid();
-    private final String assetId = faker.internet().url();
     private final ObjectMapper mapper = new ObjectMapper();
 
 
@@ -47,34 +45,22 @@ class DataRequestMessageSenderTest {
     public void setUp() {
         httpClient = niceMock(OkHttpClient.class);
 
-
         IdentityService identityService = niceMock(IdentityService.class);
         TokenResult tokenResult = niceMock(TokenResult.class);
         expect(tokenResult.getToken()).andReturn(faker.lorem().characters());
         expect(identityService.obtainClientCredentials(connectorId)).andReturn(tokenResult);
         replay(identityService, tokenResult);
-        EasyMock.capture()
-        sender = new DataRequestMessageSender("connectorId", identityService, niceMock(TransferProcessStore.class), EasyMock.mock(Vault.class), httpClient, mapper, niceMock(Monitor.class));
-
+        sender = new DataRequestMessageSender(connectorId, identityService, niceMock(TransferProcessStore.class), EasyMock.mock(Vault.class), httpClient, mapper, niceMock(Monitor.class));
     }
 
     @Test
-    public void initiateIDSMessage() {
-        var additionalProperties = Map.of(faker.internet().uuid(), faker.lorem().word(), faker.internet().uuid(), faker.lorem().word());
+    public void initiateIDSMessage() throws IOException {
+        DataRequest dataRequest = createDataRequest();
+        DataAddress dataDestination = dataRequest.getDataDestination();
 
-        String type = faker.lorem().word();
-        String secretName = faker.lorem().word();
-        Map<String, Object> destinationMap = Map.of("type", type, "keyName", secretName, "properties", new HashMap<>());
-        var dataRequest = DataRequest.Builder.newInstance()
-                .connectorId(connectorId)
-                .assetId(assetId)
-                .dataDestination(DataAddress.Builder.newInstance().keyName(secretName).type(type).build())
-                .protocol(IDS_REST)
-                .additionalProperties(additionalProperties)
-                .connectorAddress(connectorAddress)
-                .build();
-
-        expect(httpClient.newCall(matchRequest(additionalProperties, destinationMap))).andReturn(niceMock(Call.class)).times(1);
+        // record
+        Capture<Request> requestCapture = newCapture();
+        expect(httpClient.newCall(capture(requestCapture))).andReturn(niceMock(Call.class)).times(1);
         replay(httpClient);
 
         // invoke
@@ -83,39 +69,35 @@ class DataRequestMessageSenderTest {
         //verify
         verify(httpClient);
 
+        var artifactMessageRequest = getArtifactMessageRequest(requestCapture.getValue());
+        assertThat(artifactMessageRequest.getIssuerConnector()).isEqualTo(URI.create(connectorId));
+        assertThat(artifactMessageRequest.getRequestedArtifact()).isEqualTo(URI.create(dataRequest.getAssetId()));
+
+        var properties = artifactMessageRequest.getProperties();
+        assertThat((Map<String, Object>) properties.get(DESTINATION_KEY)).contains(entry("keyName", dataDestination.getKeyName()), entry("type", dataDestination.getType()));
+        assertThat(properties.get(PROPERTIES_KEY)).isEqualTo(dataRequest.getAdditionalProperties());
 
     }
 
-    public Request matchRequest(Map<String, String> additionalProperties, Map<String, Object> destinationMap) {
-        EasyMock.reportMatcher(new IArgumentMatcher() {
-            @Override
-            public boolean matches(Object argument) {
-                if (!(argument instanceof Request)) return false;
-                Map<String, Object> properties = mapper.convertValue(((Request) argument).body(), ArtifactRequestMessage.class).getProperties();
-                return properties.get(DESTINATION_KEY).equals(destinationMap) &&
-                        properties.get(PROPERTIES_KEY).equals(additionalProperties);
-            }
-
-            @Override
-            public void appendTo(StringBuffer buffer) {
-                buffer.append("OkHttp Requests don't match.");
-            }
-        });
-        return null;
-    }
-
-    @NotNull
-    private ArtifactRequestMessage createArtifactRequestMessage(Map<String, String> additionalProperties, Map<String, Serializable> destinationMap) {
-        ArtifactRequestMessage artifactRequestMessage = new ArtifactRequestMessageBuilder()
-                ._securityToken_(new DynamicAttributeTokenBuilder()
-                        ._tokenValue_(faker.lorem().word())
-                        .build())
-                ._requestedArtifact_(URI.create(assetId))
+    private DataRequest createDataRequest() {
+        var additionalProperties = Map.of(faker.internet().uuid(), faker.lorem().word(), faker.internet().uuid(), faker.lorem().word());
+        var keyName = faker.lorem().word();
+        var type = faker.lorem().word();
+        return DataRequest.Builder.newInstance()
+                .connectorId(connectorId)
+                .assetId(faker.internet().url())
+                .dataDestination(DataAddress.Builder.newInstance().keyName(keyName).type(type).build())
+                .protocol(IDS_REST)
+                .additionalProperties(additionalProperties)
+                .connectorAddress(connectorAddress)
                 .build();
+    }
 
-        artifactRequestMessage.setProperty(DESTINATION_KEY, destinationMap);
-        artifactRequestMessage.setProperty(PROPERTIES_KEY, additionalProperties);
-        return artifactRequestMessage;
+    private ArtifactRequestMessageImpl getArtifactMessageRequest(Request request) throws IOException {
+        Buffer buffer = new Buffer();
+        request.body().writeTo(buffer);
+        String json = buffer.readString(StandardCharsets.UTF_8);
+        return mapper.readValue(json, ArtifactRequestMessageImpl.class);
     }
 
 }
